8127a8827caa3396ca73b2be0a2d10937fb32258
farooq.ahmed <farahu2008@gmail.com>
2019-09-20 12:33:35 +0500

    Basic angular8 and bootstrap 4 app with page1 route.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { Page1Component } from './page1/page1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    Page1Component
   ],
   imports: [
     BrowserModule,
